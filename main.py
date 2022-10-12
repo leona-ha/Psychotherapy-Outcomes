0faadc49e52b27fc2e6fd214c2dda87b105b8728
Leona Hammelrath <50407361+leona-ha@users.noreply.github.com>
2022-10-12 11:37:50 +0200

    minor adaptations

--- main.py
+++ main.py
@@ -21,15 +21,12 @@ import pandas as pd
 from preprocessor import missing_values, aggregation_transformation, dataset_split, scaling, onehot
 from postprocessor import safe_results, compare_models
 from model import RF, NN, SVM
+from sklearn.metrics import roc_curve, auc
 
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.style.use(matplotlib.get_data_path()+'/stylelib/apa.mplstyle') # selecting the style sheet
-#import seaborn as sns
-#sns.set_theme(style="darkgrid")
-##sns.set(rc={'figure.figsize':(15,8.)})
-#sns.set(font_scale=1.5)
-#sns.set()
+
 
 IMG_SAFEPATH = os.path.join(OUTCOME_PATH, "plots")
 
@@ -57,8 +54,6 @@ if __name__ == '__main__':
     outcome_list = []
     baseline_list = []
     
-    fig = plt.figure(figsize=(6.4,4.8))
-
     for numrun in tqdm(runslist, total=ml_options["n_iterations"]):
         features_out = onehot.prepare_data(ml_options)
         X_train, X_test, y_train, y_test = dataset_split.prepare_data(ml_options, numrun, features_out)
@@ -74,22 +69,8 @@ if __name__ == '__main__':
         
         outcome_list.append(outcome_results)
 
-        
-        fpr = outcome_results[9]
-        tpr = outcome_results[10]
-        roc_auc = outcome_results[12]
 
-        print("fpr:",fpr)
-        print("tpr:",tpr)
-        print("auc:",roc_auc)
-
-    
-        plt.plot(fpr, tpr)
-        #plt.plot(fpr, tpr, label="{}, AUC={:.3f}".format(roc_auc))
-        features_out = None
-        features = None
-
-
+# Compare to baseline model
 
 
         if ml_options["baseline"] == 1:
@@ -97,24 +78,53 @@ if __name__ == '__main__':
             outcome_baseline = base.run(ml_options, X_train, X_test, y_train, y_test)
             baseline_list.append(outcome_baseline)
         
-    plt.plot([0,1], [0,1], color='orange', linestyle='--')
-
-    plt.xticks(np.arange(0.0, 1.1, step=0.1))
-    plt.xlabel("False Positive Rate")
-
-    plt.yticks(np.arange(0.0, 1.1, step=0.1))
-    plt.ylabel("True Positive Rate")
-
-    plt.title('ROC Curve Analysis')#, fontweight='bold', fontsize=15)
-    plt.legend(prop={'size':13}, loc='lower right')
-
-    img_safepath = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_roc_curves.png')
-    plt.savefig(img_safepath, dpi=300)
-        
         
     model_flatlists = safe_results.aggregate_metrics(ml_options, outcome_list, X_train, X_test)
     if ml_options["baseline"] == 1:
         compare_models.run(ml_options, baseline_list, X_train, X_test, model_flatlists)
+
+# ROC curve
+
+    fpr = model_flatlists[7]
+    tpr = model_flatlists[8]
+    roc_auc = model_flatlists[6]
+    tprs = model_flatlists[9]
+
+    fig = plt.figure(figsize=(6.4,4.8))
+
+    for i in range(len(fpr)):
+        if i == 0:
+            plt.plot(fpr[i], tpr[i], lw=1, color = 'grey', label = 'Individual Iterations')
+        else:
+            plt.plot(fpr[i], tpr[i], lw=1, color = 'grey')
+
+    mean_fpr = np.linspace(0, 1, 100)
+    tprs[-1][0] = 0.0
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[0] = 0
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(roc_auc)
+
+        
+    plt.plot(mean_fpr, mean_tpr, color='k', label=r'Mean ROC', lw=2) #plus minus: $\pm$
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='w', label='AUC = %0.2f, SD = %0.2f' % (mean_auc, std_auc))
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='k', label='Chance')
+
+    plt.legend(prop={'size':9}, loc='lower right')
+    plt.xlabel("False Positive Rate")
+    plt.xlim([-0.005, 1.005])
+
+    plt.ylabel("True Positive Rate")
+    plt.ylim([-0.005, 1.005])
+
+    img_safepath_1 = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_roc_curves.png')
+    img_safepath_2 = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_roc_curves.eps')
+
+
+    plt.savefig(img_safepath_1, dpi=300)
+    plt.savefig(img_safepath_2, dpi=1000)
+
         
     if ml_options["save_config_option"] == 1:
         savepath = os.path.join(STANDARDPATH, f'{ml_options["model_architecture"]}_configurations.csv')
@@ -131,7 +141,3 @@ if __name__ == '__main__':
         with open(savepath, 'a', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(list(ml_options.values()))
-
-
-
-    #print("elapsed_time = {}", time.time() - start_time)
--- postprocessor/safe_results.py
+++ postprocessor/safe_results.py
@@ -1,7 +1,8 @@
 import csv
 import os
 import numpy as np
-from config import STANDARDPATH, OUTCOME_PATH
+import pandas as pd
+from config import OUTCOME_PATH, DATAPATH_OUT
 import matplotlib.pyplot as plt
 import matplotlib
 
@@ -120,7 +121,8 @@ def aggregate_metrics(ml_options, input_list, X_train=None, X_test=None):
         roc_auc_mean = np.mean(roc_auc_flat)
         roc_auc_std = np.std(roc_auc_flat)
         feature_importances_count_sum = feature_importances_count_flat.sum(axis=0).reshape(1,len(input_list[0][8]))
-    
+        
+        
 
         
         number_rounds = len(accuracy_flat)
@@ -177,167 +179,43 @@ def aggregate_metrics(ml_options, input_list, X_train=None, X_test=None):
 
 
         importances = feature_importances_mean.reshape((feature_importances_mean.shape[1],))
+        importances_count = feature_importances_count_sum.reshape((feature_importances_count_sum.shape[1],))
+
         indices = np.argsort(importances)[::-1]
+        indices_count = np.argsort(importances_count)[::-1]
+
         feature_names = list(X_train.columns)
         names = [feature_names[i] for i in indices]
-        names = names[:20]
-        plt.figure(figsize=(20,15))
-        plt.title("Feature Importance")
-        plt.bar(range(X_train.shape[1])[:20], importances[indices][:20])
-        plt.xticks(range(20), names, rotation=90)
-        img_safepath = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_feature_importances.png')
+        names_count = [feature_names[i] for i in indices_count]
+        
+
+        imp_df = pd.DataFrame({"importances":importances, "names": names})
+        count_df = pd.DataFrame({"counts":importances_count, "names": names_count})
+        joint_df = imp_df.merge(count_df, on="names")
+        joint_df = joint_df.head(10)
+        print(joint_df.shape)
+
+        plt.figure(figsize=(6.4, 4.8))
+        bar_1 = plt.bar(range(X_train.shape[1])[:10], importances[indices][:10])
+        plt.xticks(range(10),joint_df["names"], rotation=45)
+        plt.xlim([-1.2, 10])
+
+        counts = joint_df["counts"]
+      
+        for i,rect in enumerate(bar_1):
+            plt.text(rect.get_x() + rect.get_width()/2.0, rect.get_height()+0.0005,f'{counts[i]:.0f}',ha='center', va='bottom', fontdict={"size":9,"fontname":"Arial"})
+
+        img_safepath = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_feature_importance.eps')
+        img_safepath_1 = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_feature_importance.png')
+        data_safepath = os.path.join(DATAPATH_OUT, f'{ml_options["model_name"]}_feature_importance.pkl')
+
+
         plt.savefig(img_safepath, dpi=300)
-
-        importance_counts = feature_importances_count_sum.reshape((feature_importances_count_sum.shape[1],))
-        indices = np.argsort(importance_counts)[::-1]
-        feature_names = list(X_train.columns)
-        names = [feature_names[i] for i in indices]
-        names = names[:20]
-        plt.figure(figsize=(20,15))
-        plt.title("Feature Counts")
-        plt.bar(range(X_train.shape[1])[:20], importance_counts[indices][:20])
-        plt.xticks(range(20), names, rotation=90)
-        plt.ylim(0, 100)
-        img_safepath = os.path.join(IMG_SAFEPATH, f'{ml_options["model_name"]}_feature_counts.png')
-        plt.savefig(img_safepath, dpi=300)
-
-### Add input list!!
-
-    elif ml_options["model_architecture"] == "SVM":
-        accuracy_min = min(accuracy_flat)
-        accuracy_max = max(accuracy_flat)
-        accuracy_mean = np.mean(accuracy_flat)
-        accuracy_std = np.std(accuracy_flat)
-        accuracy_class0_min = min(accuracy_class0_flat)
-        accuracy_class0_max = max(accuracy_class0_flat)
-        accuracy_class0_mean = np.mean(accuracy_class0_flat)
-        accuracy_class0_std = np.std(accuracy_class0_flat)
-        accuracy_class1_min = min(accuracy_class1_flat)
-        accuracy_class1_max = max(accuracy_class1_flat)
-        accuracy_class1_mean = np.mean(accuracy_class1_flat)
-        accuracy_class1_std = np.std(accuracy_class1_flat)
-        balanced_accuracy_min = min(balanced_accuracy_flat)
-        balanced_accuracy_max = max(balanced_accuracy_flat)
-        balanced_accuracy_mean = np.mean(balanced_accuracy_flat)
-        balanced_accuracy_std = np.std(balanced_accuracy_flat)
-        feature_importances_min = feature_importances_flat.min(axis=0).reshape(1,len(input_list[0][6]))
-        feature_importances_max = feature_importances_flat.max(axis=0).reshape(1,len(input_list[0][6]))
-        feature_importances_mean = feature_importances_flat.mean(axis=0).reshape(1,len(input_list[0][6]))
-        feature_importances_std = feature_importances_flat.std(axis=0).reshape(1,len(input_list[0][6]))
-    
-        number_rounds = len(accuracy_flat)
-
-        if not os.path.exists(savepath):
-            header = ['model', 'n_iterations', 'accuracy_min', 'accuracy_max', 'accuracy_mean', 'accuracy_std', 'accuracy_class0_min', \
-                'accuracy_class0_max', 'accuracy_class0_mean', 'accuracy_class0_std', 'accuracy_class1_min','accuracy_class1_max', \
-                'accuracy_class1_mean', 'accuracy_class1_std', 'balanced_accuracy_min', 'balanced_accuracy_max', 'balanced_accuracy_mean', \
-                'balanced_accuracy_std', 'feature_importances_min', 'feature_importances_max', 'feature_importances_mean', 'feature_importances_std']
+        plt.savefig(img_safepath_1, dpi=300)
+        joint_df.to_pickle(data_safepath)
 
 
-            with open(savepath, 'a', encoding='UTF8', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(header)
-                writer.writerow([ml_options["model_name"],number_rounds, accuracy_min, accuracy_max, accuracy_mean, accuracy_std, accuracy_class0_min, \
-                accuracy_class0_max, accuracy_class0_mean, accuracy_class0_std, accuracy_class1_min, accuracy_class1_max, accuracy_class1_mean, \
-                    accuracy_class1_std, balanced_accuracy_min, balanced_accuracy_max, balanced_accuracy_mean, balanced_accuracy_std,  \
-                    feature_importances_min, feature_importances_max, feature_importances_mean, feature_importances_std])
-        
-        else:  
-            with open(savepath, 'a', encoding='UTF8', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([ml_options["model_name"],number_rounds, accuracy_min, accuracy_max, accuracy_mean, accuracy_std, accuracy_class0_min, \
-                accuracy_class0_max, accuracy_class0_mean, accuracy_class0_std, accuracy_class1_min, accuracy_class1_max, accuracy_class1_mean, \
-                    accuracy_class1_std, balanced_accuracy_min, balanced_accuracy_max, balanced_accuracy_mean, balanced_accuracy_std,  \
-                    feature_importances_min, feature_importances_max, feature_importances_mean, feature_importances_std])
-
-        
-        print('Number of Rounds: ' + str(number_rounds) + 
-                '\nMin Accuracy: ' + str(accuracy_min) + '\nMax Accuracy: ' + str(accuracy_max) + '\nMean Accuracy: ' + str(accuracy_mean) + '\nStd Accuracy: ' + str(accuracy_std) +
-                '\nMin Accuracy_class_0: ' + str(accuracy_class0_min) + '\nMax Accuracy_class_0: ' + str(accuracy_class0_max) + '\nMean Accuracy_class_0: ' + str(accuracy_class0_mean) + '\nStd Accuracy_class_0: ' + str(accuracy_class0_std) +
-                '\nMin Accuracy_class_1: ' + str(accuracy_class1_min) + '\nMax Accuracy_class_1: ' + str(accuracy_class1_max) + '\nMean Accuracy_class_1: ' + str(accuracy_class1_mean) + '\nStd Accuracy_class_1: ' + str(accuracy_class1_std) +
-                '\nMin Balanced_Accuracy: ' + str(balanced_accuracy_min) + '\nMax Balanced_Accuracy: ' + str(balanced_accuracy_max) + '\nMean Balanced_Accuracy: ' + str(balanced_accuracy_mean) + '\nStd Balanced_Accuracy: ' + str(balanced_accuracy_std))
-
-        importances = feature_importances_mean.reshape((feature_importances_mean.shape[1],))
-        top_positive_coefficients = np.argsort(importances)[-10:]
-        top_negative_coefficients = np.argsort(importances)[:10]
-        top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
-        #plt.figure(figsize=(18, 7))
-        colors = ['green' if c < 0 else 'blue' for c in importances[top_coefficients]]
-        plt.bar(np.arange(2 * 10), importances[top_coefficients], color=colors)
-        feature_names = list(X_train.columns)
-        feature_names = np.array(feature_names)
-        plt.xticks(np.arange(1 + 2 * 10), feature_names[top_coefficients], rotation=45, ha='right')
-        plt.savefig(f'{ml_options["model_name"]}_feature_importances.png', dpi=300)
-    
-
-    elif ml_options["model_architecture"] == "NN":
-        for counter, sublist in enumerate(input_list):
-            for itemnumber in range(len(sublist)):
-                if itemnumber == 0:
-                    accuracy_flat.append(sublist[itemnumber])
-                elif itemnumber == 1:
-                    accuracy_class1_flat.append(sublist[itemnumber])
-                elif itemnumber == 2:
-                    accuracy_class0_flat.append(sublist[itemnumber])
-                elif itemnumber == 3:
-                    precision_flat.append(sublist[itemnumber])
-                elif itemnumber == 4:
-                    f1_score_flat.append(sublist[itemnumber])
-                elif itemnumber == 5:
-                    balanced_accuracy_flat.append(sublist[itemnumber])
-                elif itemnumber == 6:
-                    log_loss_value_flat.append(sublist[itemnumber])
-                
-        accuracy_min = min(accuracy_flat)
-        accuracy_max = max(accuracy_flat)
-        accuracy_mean = np.mean(accuracy_flat)
-        accuracy_std = np.std(accuracy_flat)
-        accuracy_class0_min = min(accuracy_class0_flat)
-        accuracy_class0_max = max(accuracy_class0_flat)
-        accuracy_class0_mean = np.mean(accuracy_class0_flat)
-        accuracy_class0_std = np.std(accuracy_class0_flat)
-        accuracy_class1_min = min(accuracy_class1_flat)
-        accuracy_class1_max = max(accuracy_class1_flat)
-        accuracy_class1_mean = np.mean(accuracy_class1_flat)
-        accuracy_class1_std = np.std(accuracy_class1_flat)
-        balanced_accuracy_min = min(balanced_accuracy_flat)
-        balanced_accuracy_max = max(balanced_accuracy_flat)
-        balanced_accuracy_mean = np.mean(balanced_accuracy_flat)
-        balanced_accuracy_std = np.std(balanced_accuracy_flat)
-        precision_min = min(precision_flat)
-        precision_max = max(precision_flat)
-        precision_mean = np.mean(precision_flat)
-        precision_std = np.std(precision_flat)
-        f1_score_min = min(f1_score_flat)
-        f1_score_max = max(f1_score_flat)
-        f1_score_mean = np.mean(f1_score_flat)
-        f1_score_std = np.std(f1_score_flat)
-        log_loss_value_min = min(log_loss_value_flat)
-        log_loss_value_max = max(log_loss_value_flat)
-        log_loss_value_mean = np.mean(log_loss_value_flat)
-        log_loss_value_std = np.std(log_loss_value_flat)
-                  
-        number_rounds = len(accuracy_flat)
-
-        if not os.path.exists(savepath):
-            with open(savepath, 'a', encoding='UTF8', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(header)
-                writer.writerow([ml_options["model_name"],number_rounds, accuracy_min, accuracy_max, accuracy_mean, accuracy_std, accuracy_class0_min, \
-                accuracy_class0_max, accuracy_class0_mean, accuracy_class0_std, accuracy_class1_min, accuracy_class1_max, accuracy_class1_mean, \
-                    accuracy_class1_std, precision_min, precision_max,precision_mean, precision_std, \
-                    f1_score_min, f1_score_max, f1_score_mean, f1_score_std, balanced_accuracy_min, balanced_accuracy_max, balanced_accuracy_mean, 
-                    balanced_accuracy_std,log_loss_value_min, log_loss_value_max, log_loss_value_mean, log_loss_value_std])
-        
-        print('Number of Rounds: ' + str(number_rounds) + 'Length X_train: ' + str(len(X_train)) + 'Length X_test: ' + str(len(X_test)) + '\nMin Accuracy: ' + str(accuracy_min) + '\nMax Accuracy: ' + str(accuracy_max) + '\nMean Accuracy: ' + str(accuracy_mean) + '\nStd Accuracy: ' + str(accuracy_std) +
-                '\nMin Accuracy_class_0: ' + str(accuracy_class0_min) + '\nMax Accuracy_class_0: ' + str(accuracy_class0_max) + '\nMean Accuracy_class_0: ' + str(accuracy_class0_mean) + '\nStd Accuracy_class_0: ' + str(accuracy_class0_std) +
-                '\nMin Accuracy_class_1: ' + str(accuracy_class1_min) + '\nMax Accuracy_class_1: ' + str(accuracy_class1_max) + '\nMean Accuracy_class_1: ' + str(accuracy_class1_mean) + '\nStd Accuracy_class_1: ' + str(accuracy_class1_std) +
-                '\nMin Balanced_Accuracy: ' + str(balanced_accuracy_min) + '\nMax Balanced_Accuracy: ' + str(balanced_accuracy_max) + '\nMean Balanced_Accuracy: ' + str(balanced_accuracy_mean) + '\nStd Balanced_Accuracy: ' + str(balanced_accuracy_std) +
-                '\nMean Precision:' + str(precision_mean) + '\nMean F1:' + str(f1_score_mean) + '\nMean Log-Loss: ' + str(log_loss_value_mean))
-    
-    model_flatlists = [accuracy_flat, accuracy_class1_flat, accuracy_class0_flat, precision_flat, f1_score_flat, balanced_accuracy_flat, roc_auc_flat]
+    model_flatlists = [accuracy_flat, accuracy_class1_flat, accuracy_class0_flat, precision_flat, f1_score_flat, balanced_accuracy_flat, roc_auc_flat, fpr_flat, tpr_flat, tprs_flat, fraction_positives_flat, mean_predicted_value_flat]
     
     return model_flatlists
 
-
- 
